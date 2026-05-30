c116cb607cf2cc2781b6a31a0948afe2ddd4ae5c
Daniel <devannetu@protonmail.com>
2026-05-30 07:07:28 +0200

    Tests: Implement runner for crypto tests

--- test.sh
+++ test.sh
@@ -1,25 +1,28 @@
 #!/bin/sh
-
 zig build
-
+export ZIG_OUT="$(pwd)/zig-out"
+: "${PARALLEL:=true}"
 : "${TOUCH:=./zig-out/bin/touch}"
 export TOUCH
 
-: "${PARALLEL:=true}"
+run_tests() {
+    dir="$1"
+    for f in "$dir"/*.sh; do
+        if [ "$PARALLEL" = "true" ]; then
+            AMPERSAND="&"
+        else
+            AMPERSAND=""
+        fi
+        if [ "$(basename "$f")" = "now-owned-by-other.sh" ]; then
+            eval "sudo -E sh \"\$f\" $AMPERSAND"
+        else
+            eval "sh \"\$f\" $AMPERSAND"
+        fi
+    done
+}
 
-for f in ./tests/touch/*.sh; do
-    if [ "$PARALLEL" = "true" ]; then
-        AMPERSAND="&"
-    else
-        AMPERSAND=""
-    fi
-
-    if [ "$(basename "$f")" = "now-owned-by-other.sh" ]; then
-        eval "sudo -E sh \"\$f\" $AMPERSAND"
-    else
-        eval "sh \"\$f\" $AMPERSAND"
-    fi
-done
+run_tests ./tests/crypto
+run_tests ./tests/touch
 
 if [ "$PARALLEL" = "true" ]; then
     wait
